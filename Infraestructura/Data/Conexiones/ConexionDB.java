3d2a64d508158f165b05655dd90074c07d409f08
enri312 <enri312@outlook.com>
2023-09-04 21:44:27 -0400

    Agendamiento

---
+++ Infraestructura/Data/Conexiones/ConexionDB.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package home.projectoagendamiento.Infraestructura.Data.Conexiones;
+
+/**
+ *
+ * @author Carlos E.N.V
+ */
+public class ConexionDB {
+    
+}
---
+++ ProjectoAgendamiento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package home.projectoagendamiento;
+
+/**
+ *
+ * @author Carlos E.N.V
+ */
+public class ProjectoAgendamiento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
